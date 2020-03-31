34d833d3c0a993a5c3e7def6fec1c4f84b4325a4
Neha Hirve <neha.s.hirve@gmail.com>
2020-03-31 20:23:00 +0530

    mobile site

--- javascript.js
+++ javascript.js
@@ -7,3 +7,16 @@ $ ( document ).ready(function(){
     $ (this).toggleClass("currentpage", loc.indexOf($(this).attr("href")) != -1 );
     });
 });
+
+
+function menu() {
+  var x = document.getElementById('index');
+  var y = document.getElementById('exhibit');
+  if (x.style.display === "block") {
+    x.style.display = "none";
+    y.style.display = "block";
+  } else {
+    x.style.display ="block";
+    y.style.display ="none";
+  };
+};
